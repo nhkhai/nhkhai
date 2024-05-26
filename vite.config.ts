39374e4bd80ba4705bf95663fff99131746c098d
Ng Heng Khai <nhkhai@gmail.com>
2024-05-27 02:54:43 +0800

    Update vite.config.ts
    Updated deployment port.

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,14 @@ export default defineConfig({
   // base: "/my-portfolio-website/",
   base: process.env.VITE_BASE_PATH || "/", // Use environment variable or default to '/'.
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "https://0.0.0.0:8080",
+  },
 });
