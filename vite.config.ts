b7c44a71ba99cf198364c6574badae33bc5c4c25
Ng Heng Khai <nhkhai@gmail.com>
2024-05-27 02:50:36 +0800

    Updated to use a custom build command for GitHub Actions.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: "/my-portfolio-website/",
-  // base: process.env.VITE_BASE || "/",
+  base: process.env.VITE_BASE_PATH || "/", // Use environment variable or default to '/'.
   plugins: [react()],
 });
