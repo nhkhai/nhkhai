c376f75ade9fc9de84f72758597dc7b36efc1912
Ng Heng Khai <nhkhai@gmail.com>
2024-05-27 02:38:35 +0800

    Updated to set the base path from the GitHub Actions yml file instead.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-portfolio-website/",
+  // base: "/my-portfolio-website/",
+  // base: process.env.VITE_BASE || "/",
   plugins: [react()],
 });
